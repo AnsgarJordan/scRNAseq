03fd18c39ae90e87b645438edba441b20c3b845c
AnsgarJordan <60148893+AnsgarJordan@users.noreply.github.com>
2022-07-07 15:42:52 -0400

    finished Seurat tutorial

--- seurat tutorial/tutorial.R
+++ seurat tutorial/tutorial.R
@@ -1,3 +1,5 @@
+
+
 library(dplyr)
 library(Seurat)
 library(patchwork)
@@ -11,3 +13,32 @@ pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
 
 # prune out the unnecessary cells (Quality Control (QC))
 pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+
+# Feature Selection
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(pbmc), 10)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(pbmc)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+# scaling - always do this before doing PCA
+
+pbmc <- ScaleData(pbmc)
+
+# PCA
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+
+# visualize PCA
+DimPlot(pbmc, reduction = "pca")
+
+
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+# using UMAP
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+DimPlot(pbmc, reduction = "umap")

4cd3a2add98bd61a2844f41d9ede3669b37c9421
AnsgarJordan <60148893+AnsgarJordan@users.noreply.github.com>
2022-07-12 12:49:15 -0400

    running UMAP on filtered data

--- seurat_filtered_data/dataPreparation.R
+++ seurat_filtered_data/dataPreparation.R
@@ -11,4 +11,20 @@ cnc
 cnc[["percent.mt"]] <- PercentageFeatureSet(cnc, pattern = "^MT-")
 
 # prune out the unnecessary cells (Quality Control (QC))
-cnc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+# cnc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+cnc <- scaleData(cnc)
+
+cnc <- JackStraw(cnc, num.replicate = 100)
+cnc <- ScoreJackStraw(cnc, dims = 1:15)
+
+# JackStrawPlot(cnc, dims = 1:15)
+ElbowPlot(cnc)
+
+## use the same number of PC's as prescribed by the elbowplot
+
+cnc <- FindNeighbors(cnc, dims = 1:12)
+cnc <- FindClusters(cnc, resolution = 0.5)
+
+cnc <- RunUMAP(cnc, dims = 1:12)
+DimPlot(cnc, reduction = "umap")
